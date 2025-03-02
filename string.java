903c09c22f53fe02f2d642b15f9a374731baec0b
Vansh Rajput <vr7404560@gmail.com>
2025-03-03 01:39:29 +0530

    modify strin

--- string.java
+++ string.java
@@ -11,8 +11,19 @@ public class string {
         String char2 = "Singh";
         String charFull = char1 +" " + char2;
         System.out.println(charFull); 
+        //substrin
         System.out.println(charFull.substring(0,6));
-
+        //length
+        System.out.println(charFull.length());
+        //comparTo
+        //if c1>c2 return positver value
+        //if c1<c2 return negative value 
+        //if c1==c2 return 0
+        if(char1.compareTo(char2)==0){
+            System.out.println(charFull);
+           } else{
+                System.out.println("Not equal");
+            }
 
 
 sc.close();
