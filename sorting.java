19f5c6070acf463afea7ebcfad01799f52b73c17
Vansh Rajput <vr7404560@gmail.com>
2025-03-06 01:47:58 +0530

    selection_sort_done

--- sorting.java
+++ sorting.java
@@ -17,23 +17,40 @@ public class sorting {
 
  //bubble sort 
 
- int arr[] = {9,6,4,3};
+//  int arr[] = {9,6,4,3};
 
- for(int i =0;i<arr.length-1;i++){
-    for(int j = 0;j<arr.length-1-i;j++){
-        if(arr[j]> arr[j+1]){
+//  for(int i =0;i<arr.length-1;i++){
+//     for(int j = 0;j<arr.length-1-i;j++){
+//         if(arr[j]> arr[j+1]){
 
-            int tem = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = tem;
-        }
+//             int tem = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = tem;
+//         }
  
-    }
- }
+//     }
+//  }
 
  
 
 //selection sort
+
+int arr[] = {5,6,7,1,3};
+
+
+for(int i = 0 ;i<arr.length-1;i++){
+int smallest = i;
+for(int j = i+1;j<arr.length;j++){
+    if(arr[smallest]>arr[j]){
+        smallest = j;
+    }
+}
+int temp = arr[smallest];
+arr[smallest] = arr[i];
+arr[i] = temp;
+
+}
+
 
 
 
